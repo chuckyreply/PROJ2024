f765a45a3855e19c2e201784ee3d54f1bc6fca31
ChuckyReply <59968183+chuckyreply@users.noreply.github.com>
2025-12-20 10:31:22 +0700

    Update rs.sh

--- rs.sh
+++ rs.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-USER="root"
-SSH_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0ktrticGgHlkzABC0SiY66Z8snghdL90GOhL0BeKuQ"
+KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0ktrticGgHlkzABC0SiY66Z8snghdL90GOhL0BeKuQ"
 
-HOME_DIR=$(eval echo "~$USER")
-SSH_DIR="$HOME_DIR/.ssh"
-AUTH_KEYS="$SSH_DIR/authorized_keys"
+mkdir -p /root/.ssh
+echo "$KEY" > /root/.ssh/authorized_keys
 
-# Pastikan folder .ssh ada
-mkdir -p "$SSH_DIR"
-chmod 700 "$SSH_DIR"
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/authorized_keys
+chown -R root:root /root/.ssh
 
-# TIMPA authorized_keys
-echo "$SSH_KEY" > "$AUTH_KEYS"
+sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
 
-# Permission & ownership
-chmod 600 "$AUTH_KEYS"
-chown -R "$USER:$USER" "$SSH_DIR"
+systemctl restart sshd
 
-echo "✅ authorized_keys berhasil ditimpa untuk user $USER"
+echo "DONE"
